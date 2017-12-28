81842569da1b1af99eb0124ae7b46844ec1f95b6
TarunJain <mailtarunjain@gmail.com>
2017-12-28 16:55:07 +0530

    Printing list elements using lambda expression

--- JavaExamples/src/main/java/LambdaExamples/ListIterationExample.java
+++ JavaExamples/src/main/java/LambdaExamples/ListIterationExample.java
@@ -1,5 +1,18 @@
 package LambdaExamples;
 
-public class ListIterationExample {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ListIterationExample {
+	public static void main(String args[]) {
+		List<String> strList = new ArrayList<String>();
+		strList.add("abcd");
+		strList.add("efgh");
+		strList.add("ijkl");
+		strList.add("mnop");
+		strList.add("qrst");
+		strList.add("uvwx");
+		strList.add("yz");
+		strList.stream().forEach(System.out::println);
+	}
 }
