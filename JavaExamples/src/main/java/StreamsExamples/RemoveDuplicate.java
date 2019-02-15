47685d3b13682dc23bdb09b20618d9917e1eab17
tarun7180 <mailtarunjain@gmail.com>
2019-02-15 18:07:22 +0530

    Create RemoveDuplicate.java

---
+++ JavaExamples/src/main/java/StreamsExamples/RemoveDuplicate.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class RemoveDuplicate {
+
+	public static void main(String[] args) {
+		List<String> testList = Arrays.asList("abcd","abcd","cdef","cdef","defg","defg","hhgf");
+		
+		System.out.println("Original list");
+		testList.stream().forEach(System.out::println);
+		
+		
+		System.out.println("List after removing duplicates");
+		testList.stream().distinct().forEach(System.out::println);
+	}
+
+}
